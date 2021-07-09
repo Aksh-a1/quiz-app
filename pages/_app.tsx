ac3c4bc46cb151225c8b1d2534b2397e918b1b8d
Aakash Maurya <akshmourya@gmail.com>
2021-07-09 15:11:02 +0530

    files renamed and contentful added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+import Head from 'next/head'
+import styles from '../styles/Home.module.scss'
 import type { AppProps } from 'next/app'
+import AppFooter from '../components/appFooter'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Let's get quizing</title>
+        <meta name='description' content='Multiple Online Quizzes' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Component {...pageProps} />
+      <AppFooter />
+    </div>
+  )
 }
+
 export default MyApp
